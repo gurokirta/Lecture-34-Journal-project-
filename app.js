5d17e31d426e119ea5e016c16554bcce1a7c7370
Guro <70147494+gurokirta@users.noreply.github.com>
2023-03-05 23:09:45 +0400

    ნაწილობრივ დავამთავრე

--- app.js
+++ app.js
@@ -31,138 +31,36 @@ function forEachStudent() {
 
     div.appendChild(divTwo);
   });
+
   contentBoxSection.append(div);
 }
 
 addButton.addEventListener("click", () => {
   forEachStudent();
-
-  const inputOne = document.getElementById("container-1");
-  inputOne.classList.add("red");
-  const inputTwo = document.getElementById("container-2");
-  inputTwo.classList.add("red");
-  const inputThree = document.getElementById("container-3");
-  inputThree.classList.add("red");
-  const inputFour = document.getElementById("container-4");
-  inputFour.classList.add("red");
-  const inputFive = document.getElementById("container-5");
-  inputFive.classList.add("red");
-  const inputSix = document.getElementById("container-6");
-  inputSix.classList.add("red");
-  const inputSeven = document.getElementById("container-7");
-  inputSeven.classList.add("red");
-  const inputEight = document.getElementById("container-8");
-  inputEight.classList.add("red");
-  const inputNine = document.getElementById("container-9");
-  inputNine.classList.add("red");
-  const inputTen = document.getElementById("container-10");
-  inputTen.classList.add("red");
-  inputOne.addEventListener("click", () => {
-    inputOne.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputOne.innerHTML < 0 ||
-      inputOne.innerHTML > 5 ||
-      isNaN(inputOne.innerHTML)
-    ) {
-      inputOne.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputTwo.addEventListener("click", () => {
-    inputTwo.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputTwo.innerHTML < 0 ||
-      inputTwo.innerHTML > 5 ||
-      isNaN(inputTwo.innerHTML)
-    ) {
-      inputTwo.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputThree.addEventListener("click", () => {
-    inputThree.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputThree.innerHTML < 0 ||
-      inputThree.innerHTML > 5 ||
-      isNaN(inputThree.innerHTML)
-    ) {
-      inputThree.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputFour.addEventListener("click", () => {
-    inputFour.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputFour.innerHTML < 0 ||
-      inputFour.innerHTML > 5 ||
-      isNaN(inputFour.innerHTML)
-    ) {
-      inputFour.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputFive.addEventListener("click", () => {
-    inputFive.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputFive.innerHTML < 0 ||
-      inputFive.innerHTML > 5 ||
-      isNaN(inputFive.innerHTML)
-    ) {
-      inputFive.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputSix.addEventListener("click", () => {
-    inputSix.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputSix.innerHTML < 0 ||
-      inputSix.innerHTML > 5 ||
-      isNaN(inputSix.innerHTML)
-    ) {
-      inputSix.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputSeven.addEventListener("click", () => {
-    inputSeven.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputSeven.innerHTML < 0 ||
-      inputSeven.innerHTML > 5 ||
-      isNaN(inputSeven.innerHTML)
-    ) {
-      inputTwo.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputEight.addEventListener("click", () => {
-    inputEight.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputEight.innerHTML < 0 ||
-      inputEight.innerHTML > 5 ||
-      isNaN(inputEight.innerHTML)
-    ) {
-      inputEight.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputNine.addEventListener("click", () => {
-    inputNine.innerHTML = Number(prompt("Input number between 0-5"));
-    while (
-      inputNine.innerHTML < 0 ||
-      inputNine.innerHTML > 5 ||
-      isNaN(inputNine.innerHTML)
-    ) {
-      inputNine.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  inputTen.addEventListener("click", () => {
-    inputTen.innerHTML = prompt("Input number between 0-5");
-    while (
-      inputTen.innerHTML < 0 ||
-      inputTen.innerHTML > 5 ||
-      isNaN(inputTen.innerHTML)
-    ) {
-      inputTen.innerHTML = prompt("Input number between 0-5");
-    }
-  });
-  if (inputTen.innerHTML > 0 || inputTen.innerHTML < 5) {
-    inputTen.classList.add("green");
-  } else {
-    inputTen.classList.add("red");
-  }
+  addScore();
 });
+
+function addScore(input) {
+  for (let i = 1; i <= 10; i++) {
+    const input = document.getElementById(`container-${i}`);
+    input.classList.add("red");
+    input.addEventListener("click", () => {
+      input.innerHTML = Number(prompt("input number between 0 - 5"));
+      while (input.innerHTML < 0 || input.innerHTML > 5) {
+        input.innerHTML = prompt("input number between 0 - 5");
+      }
+      if (
+        !isNaN(input.innerHTML) &&
+        input.innerHTML >= 0 &&
+        input.innerHTML <= 5
+      ) {
+        input.classList.replace("red", "green");
+      } else {
+        input.classList.replace("green", "red");
+      }
+    });
+  }
+}
 
 removeButton.addEventListener("click", () => {
   const lastChild = contentBoxSection.lastElementChild;
